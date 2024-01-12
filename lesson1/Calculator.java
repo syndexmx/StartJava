feea0413515adc1b593a8a119b0f03c9a906f591
Maxim Shapovalov <syndex@list.ru>
2024-01-12 19:27:44 +0300

    Final tasks for Lesson1 added

---
+++ lesson1/Calculator.java
@@ -0,0 +1,26 @@
+public class Calculator {
+
+    public static void main(String[] args) {
+        int operandOne = 17;
+        char operation = '^';
+        int operandTwo = 3;
+        int result;
+        if (operation == '+') {
+            result = operandOne + operandTwo;
+        } else if (operation == '-') {
+            result = operandOne - operandTwo;
+        } else if (operation == '*') {
+            result = operandOne * operandTwo;
+        } else if (operation == '/') {
+            result = operandOne / operandTwo;
+        } else if (operation == '%') {
+            result = operandOne % operandTwo;
+        } else {
+            result = operandOne;
+            for (int i = 2; i < operandTwo; i++) {
+                result *= operandOne;
+            }
+        }
+        System.out.println(operandOne + " " + operation + " " + operandTwo + " = " + result);
+    }
+}
---
+++ lesson1/GuessingGame.java
@@ -0,0 +1,20 @@
+import java.util.Random;
+
+public class GuessingGame {
+
+    public static void main(String[] args) {
+        Random random = new Random();
+        int target = random.nextInt(1,100);
+        int guessor = 50;
+        while (target != guessor) {
+            if (guessor > target) {
+                System.out.println("Число " + guessor + " больше того, что загадал компьютер.");
+                guessor--;
+            } else {
+                System.out.println("Число " + guessor + " меньше того, что загадал компьютер.");
+                guessor++;
+            }
+        }
+        System.out.println("Вы победили!");
+    }
+}
