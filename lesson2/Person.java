1396e1f7f1335af2caace2c1cfc1f16b09f60b09
Maxim Shapovalov <syndex@list.ru>
2024-01-15 11:15:16 +0300

    Lesson 2 Task 1

---
+++ lesson2/Person.java
@@ -0,0 +1,24 @@
+public class Person {
+
+    char sex = 'M';
+    String name = "John";
+    int heightCm = 187;
+    double weightKg = 92.3;
+    int age = 40;
+
+    public void walk() {
+        System.out.println("Walking");
+    }
+
+    public void sit() {
+        System.out.println("Sitting");
+    }
+
+    public void talk() {
+        System.out.println("Talking");
+    }
+
+    public void learnJava() {
+        System.out.println("Learning Java");
+    }
+}
